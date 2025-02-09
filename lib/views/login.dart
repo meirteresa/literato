f3b832ef8dbc6380793e28f9e41c34ee9f1e6fce
meir & teresa <meirnaa666@gmail.com>
2025-02-09 02:34:14 -0300

    Update login.dart

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -2,7 +2,7 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:literato/views/functions/decos.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:literato/controllers/controllers.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -11,23 +11,8 @@ class LoginPage extends StatefulWidget {
   _LoginPageState createState() => _LoginPageState();
 }
 
-Future<bool> verificarLogin(String email, String senha) async {
-  try {
-    // Referencie a coleção de usuários
-    final QuerySnapshot result = await FirebaseFirestore.instance
-        .collection('usuarios')
-        .where('email', isEqualTo: email)
-        .where('senha', isEqualTo: senha)
-        .get();
-
-    return result.docs.isNotEmpty;
-  } catch (e) {
-    print('Erro ao verificar login: $e');
-    return false;
-  }
-}
-
 class _LoginPageState extends State<LoginPage> {
+  final UsuarioController _controller = UsuarioController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _senhaController = TextEditingController();
   bool _senhaVisivel = false;
@@ -44,6 +29,17 @@ class _LoginPageState extends State<LoginPage> {
       ..onTap = _gotocadastro;
   }
 
+  void _updateEmailError(String? error) {
+    setState(() {
+      _emailError = error;
+    });
+  }
+  void _updateSenhaError(String? error) {
+    setState(() {
+      _senhaError = error;
+    });
+  }
+
   @override
   void dispose() {
     _emailController.dispose();
@@ -54,56 +50,9 @@ class _LoginPageState extends State<LoginPage> {
     Navigator.pushNamed(context, '/cadastro');
   }
 
-  // Validação do email
-  String? _validateEmail(String email) {
-    if (email.isEmpty) {
-      return 'O email não pode estar vazio.';
-    }
-    if (!RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$').hasMatch(email)) {
-      return 'Digite um email válido.';
-    }
-    return null;
-  }
-
-    // Validação da senha
-  String? _validateSenha(String senha) {
-    if (senha.isEmpty) {
-      return 'A senha não pode estar vazia.';
-    }
-    if (!RegExp(r'^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$').hasMatch(senha)) {
-      return 'A senha deve conter 8 caracteres, uma letra maiúscula, um número e um caractere especial.';
-    }
-    return null;
-  }
-
-  void _validateAndSubmit() async {
-    setState(() {
-      _emailError = _validateEmail(_emailController.text);
-      _senhaError = _validateSenha(_senhaController.text);
-    });
-
-    if (_emailError == null && _senhaError == null) {
-      bool loginValido = await verificarLogin(
-        _emailController.text.trim(),
-        _senhaController.text.trim(),
-      );
-
-      if (loginValido) {
-        Navigator.pushNamed(context, '/homepage');
-      } else {
-        setState(() {
-          _emailError = 'Email ou senha inválidos.';
-          _senhaError = 'Email ou senha inválidos.';
-        });
-      }
-    }
-  }
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(title: const Text("login"), backgroundColor: const Color(0xFFF9BF64)),
       backgroundColor: const Color(0xFFA0D6B6),
       resizeToAvoidBottomInset: true,
 
@@ -180,7 +129,12 @@ class _LoginPageState extends State<LoginPage> {
 
             ElevatedButton(
               style: botaoEntrar(),
-              onPressed: _validateAndSubmit,
+                onPressed: () {
+                  final email = _emailController.text;
+                  final senha = _senhaController.text;
+
+                  _controller.validarLogin(context, email, senha, _updateEmailError, _updateSenhaError);
+                },
               child: const Text('Entrar'),
             ),
 
